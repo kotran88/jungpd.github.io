e58168dba5d53ce7e6e03dc8013931065c51868b
kotran <kotran@naver.com>
2017-05-31 00:42:52 +0900

    today

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'ap-northeast-2';
 var app = express();
 var mysql=require('mysql');
-
+var fs=require('fs');
 var client=mysql.createConnection({
         host : 'o2db.cfuba8g7p6k6.ap-northeast-2.rds.amazonaws.com',
         user : 'kotran',
@@ -16,13 +16,17 @@ app.get('/form',function(req,res){
         var output=`
         <html>
                 <body>
-                        <a href="/db" >db go to from node js & aws </a>
+                        <a href="/gogo" >db go to from node js & aws </a>
                 </body>
         </html>
         `;
         res.send(output)
 });
-
+app.get("/gogo",function(req,res){
+  fs.readFile('/index.html',function(error,data){
+    res.send(data.toString())
+  })
+})
 app.get("/db",function(req,res){
         client.query('insert into test values(2, "testtesttest")',function(error,result){
         if(error){
