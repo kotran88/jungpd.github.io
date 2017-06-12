9a491a7069a45dd7b7397328c0a0af057691e3d3
DESKTOP-RLA5E4G\kotra <kotran@naver.com>
2017-06-12 23:10:45 +0900

    parameter added

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.get("/gogo",function(req,res){
   console.log("bbb")
     res.render('gogo');
 })
-app.listen(80,function(){
+
+app.get("/stat/:nickname",function(req,res){
+  console.log("nickname : "+req.params.nickname);
+  alert(req.params.nickname);
+
+})
+app.listen(8080,function(){
         console.log("connected");
 })
